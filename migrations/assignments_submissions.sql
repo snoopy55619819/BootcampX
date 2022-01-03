2f4fd201fb906f09682620d252b401935a2762ff
snoopy55619819 <nikhiltallapureddy@gmail.com>
2022-01-03 20:39:02 +0000

    Create assignment and assignment_submissions tables and populate with data.

---
+++ migrations/assignments_submissions.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS assignments; 
+DROP TABLE IF EXISTS assignment_submissions;
+
+CREATE TABLE assignments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  content TEXT,
+  day INTEGER,
+  chapter INTEGER,
+  duration INTEGER
+);
+
+CREATE TABLE assignment_submissions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  duration INTEGER,
+  submission_date DATE
+);
