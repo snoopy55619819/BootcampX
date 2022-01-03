10e6ce89faa0aaab7349f1cf75e4bb583b980d10
snoopy55619819 <nikhiltallapureddy@gmail.com>
2022-01-03 22:32:29 +0000

    Create query for total time all students in specific cohort spent on all assignments

---
+++ 2_queries_joins/2_3_total_time_all_students_in_cohort_spent_on_all_assignments.sql
@@ -0,0 +1,5 @@
+SELECT SUM(assignment_submissions.duration) AS total_duration
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'FEB12';
