e911eb25b8d213dcf264da60c64fd8d6d74cd132
snoopy55619819 <nikhiltallapureddy@gmail.com>
2022-01-04 23:59:41 +0000

    Create query for average duration of assistance requests for each cohort.

---
+++ 4_queries/4_5_average_duration_of_assistance_requests_per_cohort.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name, AVG(a_r.completed_at - a_r.started_at) AS average_assistance_time
+FROM assistance_requests a_r
+JOIN students ON students.id = a_r.student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time;
