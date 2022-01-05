d592e8a23fe1eabd95e443d459e9f15e2f2826b7
snoopy55619819 <nikhiltallapureddy@gmail.com>
2022-01-05 00:44:33 +0000

    Create query for names of all teachers that performed an assistance request during a cohort

---
+++ 4_queries/4_12_name_of_teachers_that_performed_assistance_request_during_cohort.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'JUL02'
+ORDER BY teachers.name;
